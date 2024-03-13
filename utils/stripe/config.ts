76d7a2ece55622fd08cc45d93760d00789b3b768
szy her <szymonhernik123@gmail.com>
2024-03-13 15:17:31 +0100

    receive local updates from stripe webhooks

--- utils/stripe/config.ts
+++ utils/stripe/config.ts
@@ -6,13 +6,13 @@ export const stripe = new Stripe(
     // https://github.com/stripe/stripe-node#configuration
     // https://stripe.com/docs/api/versioning
     // @ts-ignore
-    apiVersion: null,
+    apiVersion: '2023-10-16',
     // Register this as an official Stripe plugin.
     // https://stripe.com/docs/building-plugins#setappinfo
     appInfo: {
       name: 'Next.js Subscription Starter',
       version: '0.0.0',
-      url: 'https://github.com/vercel/nextjs-subscription-payments'
+      url: 'https://lc-dev-one.vercel.app'
     }
   }
 );
