7302d9fe5bc5863831158e6d05e3816616e58ef4
szy her <szymonhernik123@gmail.com>
2024-03-14 11:43:15 +0100

    try pushing to git and build to vercel

--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,7 @@ export const config = {
      * Match all request paths except:
      * - _next/static (static files)
      * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
+     * - favicon.ico (favicon file) x
      * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
      * Feel free to modify this pattern to include more paths.
      */
