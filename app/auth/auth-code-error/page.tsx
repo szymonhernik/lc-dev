58e0256d2c03723e095e15cb5dab4eacceff97e4
szy her <szymonhernik123@gmail.com>
2024-03-14 12:43:51 +0100

    Simplify auth-code-error page

--- app/auth/auth-code-error/page.tsx
+++ app/auth/auth-code-error/page.tsx
@@ -1,15 +1,3 @@
-'use client';
-import { getErrorRedirect } from '@/utils/helpers';
-import { useRouter } from 'next/navigation';
-
-export default function AuthCodeError() {
-  const router = useRouter();
-
-  return router.push(
-    getErrorRedirect(
-      '/signin',
-      'The email link is invalid or has expired.',
-      'Please try resetting your password again.'
-    )
-  );
+export default function Page() {
+  return <p>Password invalid</p>;
 }
